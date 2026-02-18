b6b69b68eebe4c7c88c0e9c1d7f269ebda7d5216
Kenny Huynh <khuynh34@toromail.csudh.edu>
2026-02-18 01:31:28 +0000

    finished

--- lab5/khuynhlab5.c
+++ lab5/khuynhlab5.c
@@ -29,6 +29,16 @@ int main(void)
 	printf("Variable z = %f \n",z);
 	printf("Variable j = %c \n",j);	
 
+	if (x==5)
+	{
+		printf("Variable x = %d \n",x);
+		printf("Variable y = %lf \ n",y);
+	}
+	else
+	{
+		puts("This puts statement can only be text");
+	}
+
 	int array[10] = {0,6,1,8,9,7,2,18,19,95};
 	int arraycount = 0;
 
