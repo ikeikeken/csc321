e51a3431a5952515ac172c2b8954bdb116c25406
Kenny Huynh <khuynh34@toromail.csudh.edu>
2026-04-09 19:30:40 +0000

    version 1

---
+++ lab9/khuynh9.c
@@ -0,0 +1,18 @@
+//Kenny Huynh
+//CSC-321
+//Lab 9
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	double input = 0;
+
+	printf("Please guess a number between 1 - 20: ");
+	scanf("%lf",&input);
+
+
+}
+
+
