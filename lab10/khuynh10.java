37928f1b11bca30c7a93475092adaf647f6eb0e6
Kenny Huynh <khuynh34@toromail.csudh.edu>
2026-04-09 19:03:49 +0000

    code complete testing now

--- lab10/khuynh10.java
+++ lab10/khuynh10.java
@@ -2,10 +2,35 @@
 //CSC321
 //Lab #10
 
+import java.util.Scanner;
+
 public class khuynh10
 {
 	public static void main(String[] args)
 	{
+		double radius = 0;
+		double height = 0;
+
+		System.out.print("Please enter the radius and height of the cylinder: ");
+		Scanner input = new Scanner(System.in);
 		
+		radius = input.nextDouble();
+		height = input.nextDouble();
+		
+		double cylindervolume = cylindervol(radius,height);
+
+		System.out.println("\nThe volume is: " + cylindervolume);`
+
+	}
+
+	static double cylindervol(double radius, double height)
+	{
+		//volume formula for a cylinder is V=πrrh
+		double volume = 0;
+		double pi = Math.PI;
+		volume = pi * radius * radius * height;
+
+		return volume;
+			
 	}
 }
