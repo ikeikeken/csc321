4dd492012ccbc250e6deb5e74bf636f031179873
Kenny Huynh <khuynh34@toromail.csudh.edu>
2026-04-11 22:39:22 +0000

    invalid number and correct number done

--- lab9/khuynh9.c
+++ lab9/khuynh9.c
@@ -4,15 +4,42 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main(void)
 {
-	double input = 0;
+	//double input = 0;
+	int input = 0;
+	int rnumber = 3;
+	bool valid = 0;
 
 	printf("Please guess a number between 1 - 20: ");
-	scanf("%lf",&input);
 
+	//scanf("%lf",&input);
+
+	scanf("%d",&input);
+
+	//printf("\nYou have entered: %lf",input);
+	
+	while (input > 20 || input < 1)
+	{
+		printf("You have entered an invalid number. \nPlease enter a valid number: ");
+		scanf("%d",&input);
+	}
+	
+	printf("You have entered: %d\n",input);
+
+	if(input == rnumber)
+	{
+		int counter = 0;
+		while (counter < rnumber)
+		{	
+			printf("You guessed correctly!\n");
+			counter++;
+		}
+	}
 
 }
+
 
 
