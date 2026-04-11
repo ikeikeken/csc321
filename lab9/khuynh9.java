e0419d453efbfa0a6c3e5d44088d00af65015007
Kenny Huynh <khuynh34@toromail.csudh.edu>
2026-04-11 23:43:48 +0000

    Correct answer done

--- lab9/khuynh9.java
+++ lab9/khuynh9.java
@@ -8,6 +8,7 @@ public class khuynh9
 {
 	public static void main(String[] args)
 	{
+		int randomnumber = 3;
 		int userinput;
 
 		System.out.print("Please guess a number between 1 - 20: ");
@@ -15,6 +16,17 @@ public class khuynh9
 
 		userinput = input.nextInt();
 
-		System.out.print("You have entered" + userinput);
+		System.out.println("You have entered " + userinput);
+		
+		if(userinput == 3)
+		{
+			int counter = 0;
+			while (counter != randomnumber)
+			{
+				System.out.println("You won and guessed the number correctly!");
+				counter++;
+			}
+		}
+		
 	}
 }
