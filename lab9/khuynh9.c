6857514d24a8922ca60cd5ffa5dc755604e46d4d
Kenny Huynh <khuynh34@toromail.csudh.edu>
2026-04-11 23:15:48 +0000

    correct, incorrect, high and low done. random number is left

--- lab9/khuynh9.c
+++ lab9/khuynh9.c
@@ -4,14 +4,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <stdbool.h>
 
 int main(void)
 {
+	srand(time(NULL));
+
 	//double input = 0;
 	int input = 0;
-	int rnumber = 3;
+	int rnumber;
 	bool valid = 0;
+
+	//rnumber = rand() % 20 +1;
+	rnumber = 3;
 
 	printf("Please guess a number between 1 - 20: ");
 
@@ -37,6 +43,14 @@ int main(void)
 			printf("You guessed correctly!\n");
 			counter++;
 		}
+	}
+	else if (input < rnumber)
+	{
+		printf("You have guessed incorrectly!\nYou lose!\nThe correct answer is higher!");
+	}
+	else
+	{
+		printf("You have guessed incorrectly!\nYou lose!\nThe correct answer is lower!\n");
 	}
 
 }
