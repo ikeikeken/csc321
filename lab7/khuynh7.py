9888208044a4f8b4dfe6829d720250a31e42e5e5
Kenny Huynh <khuynh34@toromail.csudh.edu>
2026-04-12 01:31:58 +0000

    complete

--- lab7/khuynh7.py
+++ lab7/khuynh7.py
@@ -2,4 +2,35 @@
 #CSC-321
 #Lab 7
 
+#integer
+x = 6
+
+#float
+y = 1.8
+
+#string
+z = "Noodles"
+
+#String Compare
+if (z == "Pizza"):
+    print("Variable Z is \"Pizza\"")
+elif (z == "Noodles"):
+    print ("Variable Z is \"Noodles\"")
+
+#Integer Compare
+if (x > 6):
+    print("Variable X is greater than 6")
+elif (x == 6):
+    print("Variable X is 6")
+elif (x < 6):
+    print("Variable X is less than 6")
+
+#Float Compare
+if (y > 6.18):
+    print("Variable Y is greater than 6")
+elif (y == 6.18):
+    print("Variable Y is 6.18")
+elif (y < 6.18):
+    print("Variable Y is less than 6.18")
+
 
