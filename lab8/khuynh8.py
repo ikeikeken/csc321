01d7a2e3320d4a527dd7fbafdfdfbda96c63bcb0
Kenny Huynh <khuynh34@toromail.csudh.edu>
2026-04-12 00:53:46 +0000

    greater,less,equal,if

--- lab8/khuynh8.py
+++ lab8/khuynh8.py
@@ -11,10 +11,26 @@
 #Create a while loop to print out a statement multiple times. The statement will be your favorite food.
 
 randomnumber = 3
-
+counter = 0
 #print(randomnumber)
 
 print("Please enter a number between 0 - 9: ")
-userinput = input()
-print("You have entered:",userinput)
+userinput = int(input())
+#print("You have entered:",userinput)
 
+while (userinput > 9 or userinput < 0):
+    userinput = int(input("You have entered an invalid number!\nPlease enter a valid number: "))
+
+if (userinput > randomnumber):
+    print("You have entered:",userinput,"That number is higher than my number:",randomnumber,"\nYou Lose!")
+elif (userinput < randomnumber):
+    print("You have entered:",userinput,"That number is lower than my number:",randomnumber,"\nYou Lose!")
+elif (userinput == randomnumber):
+    print("You have entered:",userinput,"\nCongratz!!! YOU WIN!!!")
+
+for i in range(7):
+    print("H3Ll0 w0rLd!")
+
+while (counter := 6):
+    print("Noodles")
+    counter++
