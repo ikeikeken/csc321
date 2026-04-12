75a230659870c5cb917499a70713ef426cdb54db
Kenny Huynh <khuynh34@toromail.csudh.edu>
2026-04-12 02:30:38 +0000

    response is needed

--- lab4/khuynh4.java
+++ lab4/khuynh4.java
@@ -15,19 +15,49 @@ public class khuynh4
 
 
 		String[] Menu ={ "Rice", "Noodles", "Coffe", "Tea"};
+		String[] Response = new String[4];
 
 		System.out.println("We have 4 items on our menu today. Please type your Selection.");
 		
 		for (counter = 0; counter < 4; counter++)
 		{
-			System.out.print(Menu[counter]+ " ");
+			System.out.print(Menu[counter] + " ");
+			
 		}
 
+		System.out.println("");
+		
 		Scanner input = new Scanner(System.in);
 		
-		userinput = input.nextString();
+		userinput = input.next();
 
 		System.out.println("You have chosen: " + userinput);
+
+		Response[0] = "That is a staple. Good choice!";
+		Response[1] = "Excellent choice that is my favorite!";
+		Response[2] = "Caffiene is the best drug";
+		Response[3] = "A weaker caffiene  source but still a caffiene source";
+
+	
+		for (counter = 0; counter < 4; counter++)
+		{
+			if (Menu[counter] == userinput)
+			{
+				System.out.println(Response[counter]);
+			}	
+		}
 		
+
+		
+		/*
+		counter = 0;
+
+		while (userinput != Menu[counter])
+		{
+			counter++;
+		}
+
+		System.out.println(Response[counter]);
+		*/
 	}
 }
