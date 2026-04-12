d51547c3aba6583d0901ca83ccf9a2bf83435db2
Kenny Huynh <khuynh34@toromail.csudh.edu>
2026-04-12 00:21:20 +0000

    initialization

---
+++ lab8/khuynh8.py
@@ -0,0 +1,16 @@
+#Kenny Huynh
+#CSC-321
+#Lab 9
+
+#Create a Python program with the following requirements.
+
+#Choose a number between 0 and 9. Ask a user to guess the number.
+#If the user's guess is lower than the number or higher than the number, let them know by displaying that information after they guess.
+#If the user guesses correctly display a win message. If the user's guess is incorrect, display a lose message.
+#Create a for loop to print out a statement multiple times. The statement can be any string of text but try to use a saying or a joke.
+#Create a while loop to print out a statement multiple times. The statement will be your favorite food.
+
+randomnumber = 3
+
+print(randomnumber)
+
