137ff2bef3596a2a597869f02e6d1972f4fa72b0
Kenny Huynh <khuynh34@toromail.csudh.edu>
2026-04-24 01:04:56 +0000

    name and length of name

--- midterm2/midterm2.c
+++ midterm2/midterm2.c
@@ -3,4 +3,15 @@
 //Midterm 2
 //04-23-26
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main (void)
+{
+	char myname[5] = "Kenny";
+
+	printf("%s\n",myname);
+	printf("length of name: %lu\n",strlen(myname));
+	return EXIT_SUCCESS;
+}
