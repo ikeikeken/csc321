35c220038609a6f41a14dc98eb6c1cb426fb78c7
Kenny Huynh <khuynh34@toromail.csudh.edu>
2026-05-07 07:43:00 +0000

    1room

---
+++ finalproject/game.c
@@ -0,0 +1,795 @@
+//Contributors
+//GP
+//Christine Pham
+//GP+
+//LS
+//Ryan Rodriguez
+//Anthony Farris
+//Charles Parker
+//Kristopher Willet
+//Albert Bautista
+//Kristopher Willett
+//Nishat Nawshin
+//Angel Martinez
+//Dagmawe (Bobby) Afework
+//Kenny Huynh
+
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+
+//place function prototypes here
+void khprinter(void);
+void ANFUN(void);
+void cparkerFun(void);
+void rgarcia_fun(void);
+void ls(void);
+void mnFun(void);
+void JMfun(void);
+void RRlab(void);
+void jwFunc(void);
+void jsl();
+void dcFun(void);
+void abFun(void);
+void function13AG(void);
+void theSigmaMethod(void);
+void jbInit(void);
+void ip21(void);
+void folksamEV(void);
+void sonionRH(void);
+void ip21(void);
+void rtFun(void);
+void JLeyva(void);
+void fish(void);
+void printStudent30(void);
+void printInitialsAjewett(void);
+void jgFun(void);
+void printInitialsSMarkos(void);
+void brooklynstitt(void);
+void PogoAM(void);
+void aaGM(void);
+void room49game(void);
+void dleyva14(void);
+void stanPush(void);
+void printdAfeworkIntials(void);
+void kwillett48(void);
+void ncInitial(void);
+
+int main(int argc, char *argv[])
+{
+	int choice = 0;
+	char name[30] = "bob";
+	srand(time(NULL));
+	
+	printf("Please enter your name: ");
+	scanf("%s",name);
+	printf("Hello %s welcome to THE RPG Game!\n",name);
+	while(choice != 99)
+	{
+		puts("You find yourself in a dark room and you are not sure how you got here.");
+		puts("As you look around you see the room has 75 doors, each labeled with a number.");
+		puts("The room starts filling with water and a loud voice tells you that you must choose a door to open and enter or you will likely drown. you may quit anytime by selecting option 99.");
+		puts("What door do you choose?");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:
+			{
+				puts("room1");
+				break;
+			}
+			case 2:
+			{
+				puts("room2");
+				break;
+			}
+			case 3:
+			{
+				cparkerFun();
+				puts("room3");
+				break;
+			}
+			case 4:
+			{
+				puts("room4");
+				break;
+			}
+			case 5:
+			{
+				puts("room5");
+				break;
+			}
+			case 6:
+			{
+				puts("room6");
+				break;
+			}
+			case 7:
+			{
+				jwFunc();
+				puts("room7");
+				break;
+			}
+			case 8:
+			{
+				folksamEV();
+				puts("room8");
+				break;
+			}
+			case 9:
+			{
+				puts("room9");
+				printInitialsAjewett();
+				break;
+			}
+			case 10:
+			{
+				fish();
+				puts("room10");
+				break;
+			}
+			case 11:
+			{
+				dleyva14();
+				puts("room11");
+				break;
+			}
+			case 12:
+			{
+				ANFUN();
+				puts("room12");
+				break;
+			}
+			case 13:
+			{
+				function13AG();
+				puts("room13");
+				break;
+			}
+			case 14:
+			{
+				dcFun();
+				puts("room14");
+				break;
+			}
+			case 15:
+			{
+				ncInitial();
+				puts("room15");
+				break;
+			}
+			case 16:
+			{
+				puts("room16");
+				break;
+			}
+			case 17:
+			{
+				jbInit();
+				puts("room17");
+				break;
+			}
+			case 18:
+			{
+				puts("room18");
+				break;
+			}
+			case 19:
+			{
+				puts("room19");
+				break;
+			}
+			case 20:
+			{
+				puts("room20");
+				break;
+			}
+			case 21:
+			{
+				ip21();
+				puts("room21");
+				break;
+			}
+			case 22:
+			{
+				puts("room22");
+				printdAfeworkIntials();
+				break;
+			}
+			case 23:
+			{
+				puts("room23");
+				break;
+			}
+			case 24:
+			{
+				JMfun();
+				puts("room24");
+				break;
+			}
+			case 25:
+			{
+
+				mnFun(); 
+				puts("room25");
+				break;
+			}
+			case 26:
+			{
+				puts("room26");
+				break;
+			}
+			case 27:
+			{
+				puts("room27");
+     				theSigmaMethod();
+				break;
+			}
+			case 28:
+			{
+				puts("room28");
+				break;
+			}
+			case 29:
+			{	
+				abFun();
+				puts("room29");
+				break;
+			}
+			case 30:
+			{
+				puts("room30");
+				printStudent30();
+				break;
+			}
+			case 31:
+			{
+				PogoAM();
+
+				puts("room31");
+				break;
+			}
+			case 32:
+			{
+				aaGM();
+				puts("room32");
+				break;
+			}
+			case 33:
+			{
+				puts("room33");
+				break;
+			}
+			case 34:
+			{
+				puts("room34");
+				break;
+			}
+			case 35:
+			{
+				sonionRH();
+				puts("room35");
+				break;
+			}
+			case 36:
+			{
+				puts("room36");
+				break;
+			}
+			case 37:
+			{
+				puts("room37");
+				break;
+			}
+			case 38:
+			{
+				puts("room38");
+				break;
+			}
+			case 39:
+			{
+				puts("room39");
+				break;
+			}
+			case 40:
+			{      
+                                ls();
+				puts("room40");
+				break;
+			}
+			case 41:
+			{
+				puts("room41");
+				break;
+			}
+			case 42:
+			{
+				jgFun();
+				puts("room42");
+				break;
+			}
+			case 43:
+			{
+				puts("room43");
+				stanPush();
+				break;
+			}
+			case 44:
+			{
+				srand(time(NULL));
+				int playerhealth = 10;
+				int playerwins = 0;
+
+				int playerdice[] = {1, 2, 3, 4, 5, 6};
+				//int npcdice[] = {1, 2, 3, 4, 5, 6};
+				int npcloadeddice[] = {2, 2, 4, 4, 6, 6};
+				int playerdiceloss = 0;
+				char playerdicechoice[7];
+				
+				khprinter();
+
+				if (playerhealth <= 0)
+				{
+					printf("You have lost all your health!\n");
+					break;
+				}
+				//puts("\nWelcome to Room  44");
+
+				printf("You entered through the door...\n Now there are more doors...\n 5 of them all lettered from A to F");
+
+
+				printf("\nYou appear into a dimly lit room with nothing but a table in the center with a person standing behind it. The room is to dark for you to see their face put they invite you with gestures to the table. It appears to be a game of dice.");
+				printf("\nThe stranger shows you your dice and theirs. You decide to try it.");
+				
+				while (playerhealth > 0)
+				{	
+					printf("\nEnter your choice: \"roll\" or \"observe\" or \"return\"\n\n");
+					scanf("%s", playerdicechoice);
+					printf("\nYou chose: %s\n", playerdicechoice);
+
+					
+					if (strcmp(playerdicechoice, "return") == 0)
+					{
+						break;
+					}
+					else if (strcmp(playerdicechoice, "roll") == 0)
+				
+					{
+   						printf("You roll!\n");
+						
+						int playerroll = playerdice[rand() % 6];
+						int npcroll = npcloadeddice[rand() % 6];
+						printf("You rolled: %d\n", playerroll);
+						printf("Opponent rolled: %d\n", npcroll);
+
+						if (playerroll > npcroll)
+						{
+							printf("You win this round!\n");
+							playerwins++;
+						}
+						else if (playerroll < npcroll)
+						{
+							printf("You lose this round!\n");
+							playerhealth--;
+							playerdiceloss++;
+						}
+						else
+						{
+							printf("It's a tie!\n");
+						}
+						
+						printf("Health: %d | Wins: %d\n", playerhealth, playerwins);
+
+						if (playerhealth <= 0)
+						{
+							printf("You have lost all your health!\n");
+							break;
+						}
+					}
+				
+					else if (strcmp(playerdicechoice, "observe") == 0)
+				
+					{
+    						printf("You observe...\n");
+
+						if (playerdiceloss <= 2)
+						{
+							printf("Everything looks normal\n");
+						}
+						else if (playerdiceloss <= 4)
+						{
+							printf("You notice the opponents dice always land on even numbers.\n");
+						}
+						else (playerdiceloss >=6)
+						{
+							printf("THOSE ARE LOADED DICE!!!\n");
+						}
+					}
+				
+					else
+				
+					{
+				       		printf("Invalid choice. Please enter a correct one. \n");
+					}
+				
+				}
+				
+				break;
+			}
+			case 45:
+			{
+				puts("room45");
+				brooklynstitt();
+				break;
+			}
+			case 46:
+			{
+				puts("room46");
+				break;
+			}
+			case 47:
+			{
+				rgarcia_fun();
+				puts("room47");
+				break;
+			}
+			case 48:
+			{
+				kwillett48();
+				puts("room48");
+				break;
+			}
+			case 49:
+			{
+				puts("room49");
+				room49game();
+				break;
+			}
+			case 50:
+			{
+				puts("room50");
+				break;
+			}
+			case 51:
+			{
+				puts("room51");
+				break;
+			}
+			case 52:
+			{
+				puts("room52");
+				break;
+			}
+			case 53:
+			{
+				puts("room53");
+				break;
+			}
+			case 54:
+			{
+				puts("room54");
+				break;
+			}
+			case 55:
+			{
+				puts("room55");
+				break;
+			}
+			case 56:
+			{
+                                JLeyva();
+				puts("room56");
+                                break;
+			}
+			case 57:
+			{
+				rtFun();
+                                puts("room57");
+                                break;
+			}
+			case 58:
+			{
+				printInitialsSMarkos();
+                                puts("room58");
+                                break;
+			}
+			case 59:
+			{
+				jsl();
+                                puts("room59");
+                                break;
+			}
+			case 60:
+			{
+				puts("room60");
+                                break;
+			}
+			case 61:
+			{
+				RRlab();
+                                puts("room61");
+                                break;
+			}
+			case 62:
+			{
+                                puts("room62");
+                                break;
+			}
+			case 63:
+			{
+                                puts("room63");
+                                break;
+			}
+			case 64:
+			{
+                                puts("room64");
+                                break;
+			}
+			case 65:
+			{
+                                puts("room65");
+                                break;
+			}
+			case 66:
+			{
+                                puts("room66");
+                                break;
+			}
+			case 67:
+			{
+                                puts("room67");
+                                break;
+			}
+			case 68:
+			{
+                                puts("room68");
+                                break;
+			}
+			case 69:
+                        {
+                                puts("room69");
+                                break;
+                        }
+			case 70:
+                        {
+                                puts("room70");
+                                break;
+                        }
+			case 71:
+                        {
+                                puts("room71");
+                                break;
+                        }
+			case 72:
+                        {
+                                puts("room72");
+                                break;
+                        }
+			case 73:
+                        {
+                                puts("room73");
+                                break;
+                        }
+			case 74:
+                        {
+                                puts("room74");
+                                break;
+                        }
+			case 75:
+                        {
+                                puts("room75");
+                                break;
+                        }
+			case 99:
+			{
+				puts("You have escaped");
+				break;
+			}
+			default:
+			{
+				puts("invalid choice");
+			}
+		}
+	}
+	puts("Game Over");
+	return EXIT_SUCCESS;
+}
+
+//place function definitions below this comment
+
+
+void room49game(void)
+{
+	printf("G.C\n");
+}
+
+void mnFun(void)
+{
+	puts("MNroom25");
+}
+
+
+
+//place functions here
+//
+void cparkerFun(void)
+{
+	printf("C P\n");
+}
+void rgarcia_fun(void)
+{
+	printf("RJG");
+}
+
+
+
+void ls(void)
+{
+	printf("LS\n");
+       
+}
+void JMfun(void)
+{
+	printf("JM");
+}
+
+
+void RRlab(void)
+{
+	printf("RRroom61\n");
+}
+
+
+void jwFunc(void)
+{
+	printf("JW\n");
+}
+
+
+
+void jsl(void)
+{
+	printf("JSL\n");
+}
+
+void khprinter(void)
+{
+	printf("KDH");
+}
+
+
+void PogoAM(void)
+{
+	printf("AM\n");
+}
+void dcFun(void)
+{
+	printf("DC\n");
+}
+
+
+
+
+void function13AG(void)
+{
+	printf("AG\n");
+}
+void abFun(void)
+{
+	printf("AB");
+}
+
+
+
+void theSigmaMethod(void)
+{
+  printf("J.Q.\n");
+
+}
+
+
+void folksamEV(void) 
+{
+	printf("EV\n");
+}
+
+void sonionRH(void) 
+{
+	printf("RAOOL-HIG\n");
+}
+
+void jbInit()
+{
+	puts("jbRoom17");
+}
+
+// Case 45:
+void brooklynstitt(void)
+{
+	printf("Initials: BAS\n\n");
+}
+
+void ip21(void)
+{
+	printf("IP\n");
+}
+
+void rtFun(void)
+{
+	printf("RT");
+}
+
+void JLeyva(void)
+{
+	printf("JL");
+}
+
+void fish(void) 
+{
+	printf("JF");
+
+}
+
+void printStudent30(void)
+{
+	printf("Student initials CP\n");
+}
+
+void printInitialsAjewett(void)
+{
+	printf("AJ\n");
+}
+
+void jgFun(void)
+{
+	printf("JGB");	
+
+}
+
+void printInitialsSMarkos(void) 
+{
+	printf("SM\n");
+}
+
+void aaGM(void)
+{
+	printf("AA\n");
+}
+
+void ANFUN(void)
+{
+    printf("AN");
+}
+void dleyva14(void)
+{
+	printf("DL\n");
+}
+
+void printdAfeworkIntials(void)
+{
+	puts("DA");
+}
+
+
+void kwillett48(void)
+{
+	printf("KDW");
+}
+
+void ncInitial(void)
+{
+	printf("NC");
+}
+
+
+
+void stanPush(void)
+{
+	puts(" S J \n");
+}
+
